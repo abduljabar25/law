c18c202f1c29ec520885be9051017359665439a6
abd <abd@localhost.localdomain>
2020-03-01 18:31:24 +0330

    your message goes here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AdMobFree, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,21 +13,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'الرئيسية',
       url: '/home',
       icon: 'home'
     },
     {
-      title: 'List',
+      title: 'عن التطبيق',
       url: '/list',
-      icon: 'list'
+      icon: 'alert'
     }
   ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,public admob: AdMobFree ,
   ) {
     this.initializeApp();
   }
@@ -35,6 +36,24 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      
     });
   }
+  ionViewDidLoad() {
+    if(this.platform.is('cordova')) { 
+    const bannerConfig: AdMobFreeInterstitialConfig = {  
+      id : 'ca-app-pub-9531015139305233/8826608321',
+      isTesting: false, // Remove in production  
+        autoShow: true  ,
+         
+    };  
+    this.admob.banner.config(bannerConfig);
+
+		this.admob.banner.prepare()
+                .then(()=>{
+                       this.admob.banner.show();
+                  })
+               .catch(e => console.log(e));
+}
+}
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Platform} from '@ionic/angular';
+import { AdMobFree, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free/ngx';  
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public admob: AdMobFree , public platform: Platform) {}
+  ionViewDidLoad() {
+    if(this.platform.is('cordova')) { 
+    const bannerConfig: AdMobFreeInterstitialConfig = {  
+      id : 'ca-app-pub-9531015139305233/8826608321',
+      isTesting: false, // Remove in production  
+        autoShow: true  ,
+         
+    };  
+    this.admob.banner.config(bannerConfig);
 
+		this.admob.banner.prepare()
+                .then(()=>{
+                       this.admob.banner.show();
+                  })
+               .catch(e => console.log(e));
+}
+}
 }
